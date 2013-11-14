c5e416f8fa2300c7d8da38f4180aa24ac9eefde3
ptorchilov <ptorchilov@gmail.com>
2013-11-14 04:27:16 +0300

    add bind for server address

--- lab02/main.c
+++ lab02/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int parsePort(char* portString)
+int parse_port(char* port_string)
 {
     char* end;
     const short radix = 10;
 
-    long port = strtol(portString, &end, radix);
+    long port = strtol(port_string, &end, radix);
 
-    if (end != strchr(portString, '\0') || port < 0 || port > 65535) {
+    if (end != strchr(port_string, '\0') || port < 0 || port > 65535) {
         printf("Invalid value for port.\n");
         exit(1);
     }
@@ -21,10 +24,30 @@ int main(int argc, char* argv[])
 {
     if (argc < 2) {
         printf("Enter port for listening.\n");
+
         return 2;
     }
 
-    int port = parsePort(argv[1]);
+    int port = parse_port(argv[1]);
+    int listener = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (listener < 0) {
+        printf("Error in socket creating.\n");
+
+        return 3;
+    }
+
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listener, (struct sockaddr*) &server_address, sizeof(server_address)) < 0) {
+        printf("Error in binding server address.\n");
+
+        return 4;
+    }
 
     return 0;
 }
