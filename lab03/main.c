1b64f9a5d18fd04bbd1f94c75f833429c4476f70
ptorchilov <ptorchilov@gmail.com>
2013-12-03 21:56:16 +0300

    Initial commit. Add main function and function for show errors.

---
+++ lab03/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_error(char* message, int error_code) {
+    fprintf(stderr, message);
+    exit(error_code);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3 || argc > 4) {
+        print_error("Invalid params.\nUsage: [hostname] [port] <file path>", 1);
+    }
+
+
+
+    return 0;
+}
