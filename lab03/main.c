8d0e7a7d51c17b328829c97affb05845a6e06515
ptorchilov <ptorchilov@gmail.com>
2013-12-04 03:23:53 +0300

    Fix syntax errors.

--- lab03/main.c
+++ lab03/main.c
@@ -7,9 +7,12 @@
 #include <unistd.h>
 #include <netdb.h>
 
+#define BUFFER_SIZE 2048
+#define ADDITIONAL_BUFFER_SIZE 256
 
-void send_file(char* host, int port, int file_path);
+void send_file(char* host, int port, char* file_path);
 void print_error(char* message, int error_code);
+long get_file_size(FILE* file);
 
 int client_socket_descriptor = -1;
 int server_socket_descriptor = -1;
@@ -25,7 +28,7 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void send_file(char* host, int port, int file_path) {
+void send_file(char* host, int port, char* file_path) {
     struct sockaddr_in server_socket;
 
     memset(&server_socket, 0, sizeof(server_socket));
@@ -48,7 +51,7 @@ void send_file(char* host, int port, int file_path) {
         print_error("Can't create socket", 3);
     }
 
-    if (connect(client_socket_descriptor, (struct sockaddr*) server_socket,
+    if (connect(client_socket_descriptor, (struct sockaddr*) &server_socket,
                 sizeof(server_socket)) < 0) {
         print_error("Error in connect", 4);
     }
@@ -58,6 +61,17 @@ void send_file(char* host, int port, int file_path) {
     if (file == NULL) {
         print_error("Can't open file in r+ mode", 5);
     }
+
+    long file_size = get_file_size(file);
+    char header_buffer[256];
+
+    sprintf(header_buffer, "%s:%ld", basename(file_path), file_size);
+    if (send(client_socket_descriptor, header_buffer, sizeof(header_buffer), 0) == -1) {
+        print_error("Send header error", 6);
+    }
+
+
+
 }
 
 void print_error(char* message, int error_code) {
@@ -72,7 +86,7 @@ long get_file_size(FILE* file) {
 
     long full_size = ftell(file);
 
-    fseek(file, pos, SEEK_SET);
+    fseek(file, current_position, SEEK_SET);
 
     return full_size;
 }
